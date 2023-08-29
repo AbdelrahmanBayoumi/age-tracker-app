1a8513261e6d3c0c136a2ff1b262a4f87d201d3f
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2023-08-29 21:44:09 +0300

    show success alert after add

--- src/app/home/add-birthday/add-birthday.component.ts
+++ src/app/home/add-birthday/add-birthday.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import Swal from 'sweetalert2';
 
 import * as fromApp from '../../store/app.reducer';
 import * as BirthdayActions from '../../birthday/store/birthday.actions';
-import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-add-birthday',
@@ -20,11 +22,19 @@ export class AddBirthdayComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.storeSub = this.store
       .select('birthdays')
-      .subscribe((birthdayState) => {
+      .subscribe(async (birthdayState) => {
         if (this.isLoading === true) {
           if (birthdayState.errMsg === '' && birthdayState.loading === false) {
             this.isLoading = birthdayState.loading;
             this.errorMessage = birthdayState.errMsg;
+            await Swal.fire({
+              title: 'Birthday added successfully!',
+              icon: 'success',
+              showConfirmButton: false,
+              timer: 3000,
+              position: 'center',
+            });
+
             this.router.navigate(['/']);
             return;
           }

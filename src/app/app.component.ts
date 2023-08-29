59e0eac262b0999c53eb29ca2b9a948f0d52f871
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2023-08-29 23:55:43 +0300

    add `no birthdays` placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
-import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,7 +10,7 @@ import { Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   private userSub: Subscription | undefined;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.userSub = this.authService.autoLogin()?.subscribe();
